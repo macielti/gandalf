4804d1052890157a400f52d3b5580877a7bdcac4
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-04-13 08:21:03 -0300

    add UserIdentity entity class

---
+++ lib/features/authentication/domain/entities/user_identity.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class UserIdentity extends Equatable {
+  UserIdentity({
+    required this.id,
+    required this.roles,
+    required this.username,
+    required this.token,
+  });
+
+  final String id;
+  final List<String> roles;
+  final String username;
+  final String token;
+
+  @override
+  List<Object?> get props => [id, roles, username];
+}
