e2e214a663ccd2727e7945bf182709aa17d7f1c7
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-04-20 19:30:49 -0300

    integrate with container injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:gandalf/features/authentication/presentation/pages/login_page.dart';
+import 'package:provider/provider.dart';
+import 'features/authentication/presentation/bloc/user_bloc.dart';
+import 'injection_container.dart' as di;
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Gandalf',
-    theme: ThemeData(primaryColor: Colors.black),
-    home: LoginPage(),
-  ));
+void main() async {
+  await di.init();
+
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => di.sl<UserBloc>())],
+      child: MaterialApp(
+        title: 'Gandalf',
+        theme: ThemeData(primaryColor: Colors.black),
+        home: LoginPage(),
+      ),
+    ),
+  );
 }
