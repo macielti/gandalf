2808ddc0b953462d5273f85413488b9d2182c064
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-04-19 21:24:27 -0300

    added initial code for use identity data source

---
+++ lib/features/authentication/data/datasources/user_identity_data_source.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+abstract class UserIdentityDataSource {
+  Future<void> create(String token);
+}
+
+class UserIdentityDataSourceImpl implements UserIdentityDataSource {
+  UserIdentityDataSourceImpl(this.storage);
+
+  final FlutterSecureStorage storage;
+
+  @override
+  Future<void> create(String token) async {
+    storage.write(key: 'token', value: token);
+  }
+}
