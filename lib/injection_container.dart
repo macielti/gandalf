4c7c189d7d6f378502501c20f92efa4a6500dcf2
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-04-20 19:25:54 -0300

    add injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,8 +1,15 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:gandalf/features/authentication/data/datasources/user_data_source.dart';
+import 'package:gandalf/features/authentication/data/datasources/user_identity_data_source.dart';
+import 'package:gandalf/features/authentication/data/repositories/user_identity_repository_impl.dart';
 import 'package:gandalf/features/authentication/domain/repositories/user_identity_repository.dart';
 import 'package:gandalf/features/authentication/domain/usecases/create_user_identity.dart';
 import 'package:gandalf/features/authentication/presentation/bloc/user_bloc.dart';
+import 'package:http/http.dart' as http;
 import 'package:get_it/get_it.dart';
 
+import 'features/authentication/data/repositories/user_repository_impl.dart';
+import 'features/authentication/domain/repositories/user_repository.dart';
 import 'features/authentication/domain/usecases/create_user.dart';
 
 final sl = GetIt.instance;
@@ -10,9 +17,22 @@ final sl = GetIt.instance;
 Future<void> init() async {
   // --Blocs
   sl.registerFactory(() => UserBloc(sl()));
+
   // --Use cases
   sl.registerLazySingleton(() => CreateUserIdentity(sl()));
   sl.registerLazySingleton(() => CreateUser(sl()));
+
   // --Repositories
-  sl.registerLazySingleton<UserIdentityRepository>(() => null);
+  sl.registerLazySingleton<UserIdentityRepository>(
+      () => UserIdentityRepositoryImpl(sl()));
+  sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(sl()));
+
+  // --Data sources
+  sl.registerLazySingleton<UserIdentityDataSource>(
+      () => UserIdentityDataSourceImpl(sl()));
+  sl.registerLazySingleton<UserDataSource>(() => UserDataSourceImpl(sl()));
+
+  // --External
+  sl.registerFactory(() => http.Client());
+  sl.registerFactory(() => FlutterSecureStorage());
 }
