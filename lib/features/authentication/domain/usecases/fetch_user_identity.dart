d9d9a4872395689473caaea31220b683b13dfaa5
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-04-21 16:32:13 -0300

    add fetch user identity use case class implementation

---
+++ lib/features/authentication/domain/usecases/fetch_user_identity.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:gandalf/core/error/failures.dart';
+import 'package:gandalf/core/usecases/usecase.dart';
+import 'package:gandalf/features/authentication/domain/entities/user_identity.dart';
+import 'package:gandalf/features/authentication/domain/repositories/user_identity_repository.dart';
+
+class FetchUserIdentity implements UseCase<UserIdentity, NoParams> {
+  FetchUserIdentity(this.repository);
+
+  final UserIdentityRepository repository;
+
+  @override
+  Future<Either<Failure, UserIdentity>> call(NoParams params) async {
+    return await repository.fetch();
+  }
+}
